5b4c329b7830ba2fa4d435b745a5b47e347bf543
bilibilijin <544378003@qq.com>
2024-04-09 17:34:41 +0800

    commit:fixed the empty table && virtual_list

---
+++ packages/icons/docs/demos/basic.vue
@@ -0,0 +1,22 @@
+<docs>
+---
+title: 图标
+---
+
+图标基本用法
+</docs>
+
+<!--  -->
+<script setup lang='ts'>
+import { Loading } from '@jin-ui/icons'
+</script>
+
+<template>
+  <div>
+    <Loading />
+  </div>
+</template>
+
+<style  scoped>
+
+</style>
